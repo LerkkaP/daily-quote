cbab40c308826fa0d2296f9e9d62d9baad59e38f
Erik Peteri <peteri.erik@gmail.com>
2024-01-02 14:34:02 +0200

    improve styling

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,20 +1,42 @@
-import { useQuote } from "../services/quote";
+//import { useQuote } from "../services/quote";
 
 const Card = () => {
-  const { isPending, error, data } = useQuote();
+  /*const { isPending, error, data } = useQuote();
 
   if (isPending) return 'Loading...'
 
   if (error) return 'An error has occurred: '
 
-  const quote = data[0];
+  const quote = data[0];*/
+
+  const quote: string = "He who cheats the earth will be cheated by the earth.";
+
+  const author: string = "Chinese Proverb";
 
   return (
-    <div>
-      <h1>{quote.quote}</h1>
+    <div className="h-screen flex flex-col items-center justify-center italic">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="px-6 py-10">
+          <blockquote>
+            &ldquo;{quote}&rdquo;
+            <footer className="text-right">&mdash; {author}</footer>
+          </blockquote>
+        </div>
+      </div>
+      <div>
+        <p className="mt-10 hover:scale-110 transition duration-500 object-cover">
+          Inspirational quotes provided by{" "}
+          <a
+            className="underline underline-offset-4"
+            href="https://zenquotes.io/"
+            target="_blank"
+          >
+            ZenQuotes API
+          </a>
+        </p>
+      </div>
     </div>
-    )
-  }
-
+  );
+};
 
 export default Card;
