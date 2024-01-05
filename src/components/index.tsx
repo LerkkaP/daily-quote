f4261e4b2531dcb0fa395a8503fe7d7a197170c7
Erik Peteri <peteri.erik@gmail.com>
2024-01-05 20:50:24 +0200

    add state for managing mode

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,8 +1,10 @@
 //import { useQuote } from "../services/quote";
+import { useState } from "react";
 
 const Card = () => {
+  const [darkMode, setDarkMode] = useState(false)
+  document.documentElement.classList.add('dark');
   /*const { isPending, error, data } = useQuote();
-
   if (isPending) return 'Loading...'
 
   if (error) return 'An error has occurred: '
@@ -15,6 +17,11 @@ const Card = () => {
 
   return (
     <div className="dark h-screen flex flex-col items-center justify-center font-playfair">
+      <label className="relative inline-flex items-center cursor-pointer pb-5">
+        <input type="checkbox" value="" onClick={() => setDarkMode(!darkMode)} className="sr-only peer"/>
+        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+      </label>
       <div className="max-w-sm rounded-md overflow-hidden shadow-lg">
         <div className="px-6 py-10">
           <blockquote>
