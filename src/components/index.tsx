3107eb36a70b8e10ad9e5da8981cac15cb166d0e
Erik Peteri <peteri.erik@gmail.com>
2024-06-02 16:30:50 +0300

    add useEffect fetching

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,26 @@
 import { useState, useEffect } from "react";
+import { getQuote } from "../services/quote";
 
 const Card = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const quoteData = await getQuote();
+        setData(quoteData);
+        setIsPending(false);
+      } catch (err) {
+        setError(err.message);
+        setIsPending(false);
+      }
+    };
+
+    fetchQuote();
+  }, []);
 
   useEffect(() => {
     if (darkMode) {
@@ -11,11 +30,15 @@ const Card = () => {
     }
   }, [darkMode]);
 
-  /*const quote: string = data[0]["q"];
-  const author: string = data[0]["a"];*/
+  if (isPending) return "Loading...";
+
+  if (error) return `An error has occurred: ${error}`;
+
+  const quote = data[0]["q"];
+  const author = data[0]["a"];
 
   return (
-    <div className="leading-relaxed bg-white dark:bg-gray-800 h-screen flex flex-col items-center justify-center font-playfair">
+    <div className="leading-relaxed bg-slate-50 dark:bg-gray-800 h-screen flex flex-col items-center justify-center font-playfair">
       <label className="relative inline-flex items-center cursor-pointer pb-5">
         <input
           type="checkbox"
@@ -23,10 +46,10 @@ const Card = () => {
           onClick={() => setDarkMode(!darkMode)}
           className="sr-only peer"
         />
-        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none  dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
         <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
       </label>
-      <div className="max-w-sm rounded-md overflow-hidden shadow-lg dark:bg-gray-700	">
+      <div className="max-w-sm rounded-md overflow-hidden shadow-lg dark:bg-gray-700">
         <div className="px-6 py-10 dark:text-white">
           <blockquote>
             &ldquo;{quote}&rdquo;
--- src/services/quote.ts
+++ src/services/quote.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { Quote } from "../types";
 
 const url = "http://localhost:3000/quote";
+
 export const getQuote = async () => {
   const { data } = await axios.get<Quote[]>(url);
   return data;
